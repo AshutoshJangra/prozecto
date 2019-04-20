c67d5d0bbb40d7a290e39b06546e92bf6ca7da8a
Ashutosh Jangra <ashutoshjangra603@gmail.com>
2019-04-20 13:42:37 +0530

    Updates

---
+++ precache-manifest.b5223e7d92714cb37d417d992f80b023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76f33605c44c38d8560497f3e7d35612",
+    "url": "/prozecto/static/media/photo.76f33605.png"
+  },
+  {
+    "revision": "6b38f69420a2dee36a46",
+    "url": "/prozecto/static/js/runtime~main.6b38f694.js"
+  },
+  {
+    "revision": "3ed09c4fd62a2b5fa6f4",
+    "url": "/prozecto/static/js/main.3ed09c4f.chunk.js"
+  },
+  {
+    "revision": "f9ab8b9c398b2111c874",
+    "url": "/prozecto/static/js/2.f9ab8b9c.chunk.js"
+  },
+  {
+    "revision": "3ed09c4fd62a2b5fa6f4",
+    "url": "/prozecto/static/css/main.5fe14604.chunk.css"
+  },
+  {
+    "revision": "de5f71e24db88b7936d3e6c9adee1e99",
+    "url": "/prozecto/index.html"
+  }
+];
